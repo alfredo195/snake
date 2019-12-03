16c37c5983413417970c0209313c3ae6493fa4c2
BlackAce <alfredo.gon195@gmail.com>
2019-12-03 14:03:07 -0800

    snake can move

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-const CANVAS_BORDER_COLOUR = 'black';
+const CANVAS_BORDER_COLOUR = "black";
 const CANVAS_BACKGROUND_COLOUR = "white";
 
 const gameCanvas = document.getElementById("gameCanvas");
 
 const ctx = gameCanvas.getContext("2d");
 
-let snake = [  {x: 150, y: 150},  {x: 140, y: 150},  {x: 130, y: 150},  {x: 120, y: 150},  {x: 110, y: 150},];
+let snake = [
+  { x: 150, y: 150 },
+  { x: 140, y: 150 },
+  { x: 130, y: 150 },
+  { x: 120, y: 150 },
+  { x: 110, y: 150 }
+];
 
-function drawSnakePart(snakePart) {  ctx.fillStyle = 'lightgreen';  ctx.strokestyle = 'darkgreen';
+function drawSnakePart(snakePart) {
+  ctx.fillStyle = "lightgreen";
+  ctx.strokestyle = "darkgreen";
+  ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
+  ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+}
+
+function drawSnake() {  snake.forEach(drawSnakePart);}

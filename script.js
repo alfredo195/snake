9de51502d0526bd90596ad18820c3ecf5a02a9fa
BlackAce <alfredo.gon195@gmail.com>
2019-12-03 13:28:01 -0800

    added snake

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-document.getElementById("gameCanvas")
+const CANVAS_BORDER_COLOUR = 'black';
+const CANVAS_BACKGROUND_COLOUR = "white";
+
+const gameCanvas = document.getElementById("gameCanvas");
+
+const ctx = gameCanvas.getContext("2d");
+
+let snake = [  {x: 150, y: 150},  {x: 140, y: 150},  {x: 130, y: 150},  {x: 120, y: 150},  {x: 110, y: 150},];
+
+function drawSnakePart(snakePart) {  ctx.fillStyle = 'lightgreen';  ctx.strokestyle = 'darkgreen';
